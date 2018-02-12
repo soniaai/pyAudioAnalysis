f72f6cc9366e5ab769036d2125d575bf06cb103f
Rimvydas Naktinis <naktinis@gmail.com>
2018-02-12 22:51:20 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='pyAudioAnalysis',
+    version='0.1.4-naktinis',
+
+    description='Python Audio Analysis Library: Feature Extraction, Classification, Segmentation and Applications',
+    long_description='pyAudioAnalysis is a Python library covering a wide range of audio analysis tasks, including: feature extraction, classification, segmentation and visualization.',
+    url='https://github.com/tyiannak/pyAudioAnalysis',
+    author='Theodoros Giannakopoulos',
+    author_email='tyiannak@gmail.com',
+    license='Apache 2.0',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    keywords='audio analysis feature extraction classification segmentation visualization',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    install_requires=['numpy', 'matplotlib', 'scipy', 'sklearn', 'hmmlearn', 'simplejson', 'eyed3', 'pydub'],
+)
